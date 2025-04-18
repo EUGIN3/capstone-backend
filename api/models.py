1aaac6677c007a564407dab11b7c41781aec17fe
eugine <eugine2468@gmail.com>
2025-04-18 15:40:53 +0800

    Custom user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is a required field!')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+    
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=250, unique=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+
+    # Not required fields in registration
+    first_name  = models.CharField(max_length=500, null=True, blank=True)
+    last_name = models.CharField(max_length=500, null=True, blank=True)
+
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    facebook_link = models.URLField(max_length=500, null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
